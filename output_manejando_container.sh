b2f1250accf4c1e00ad37b78173c05831501e6d7
Mario Pimienta Calderon <m.pimientacalderon@um.es>
2022-01-28 18:24:44 +0100

    Manejando container

---
+++ output_manejando_container.sh
@@ -0,0 +1,38 @@
+-----APARTADO A-----
+PRETTY_NAME="Debian GNU/Linux 10 (buster)"
+NAME="Debian GNU/Linux"
+VERSION_ID="10"
+VERSION="10 (buster)"
+VERSION_CODENAME=buster
+ID=debian
+HOME_URL="https://www.debian.org/"
+SUPPORT_URL="https://www.debian.org/support"
+BUG_REPORT_URL="https://bugs.debian.org/"
+-----APARTADO B-----
+total 64
+-rwxrwxr-x. 1 alumno19 alumno19 18336 Jan 26 23:29 k-mer13
+-rwxrwxr-x. 1 alumno19 alumno19  1434 Jan 26 23:27 k-mer13.cpp
+-rwxrwxr-x. 1 alumno19 alumno19   948 Jan 26 23:28 k-mer13.py
+-rwxrwxr-x. 1 alumno19 alumno19 18280 Jan 26 23:30 k-mer14
+-rwxrwxr-x. 1 alumno19 alumno19  1169 Jan 26 23:33 k-mer14.cpp
+-rwxrwxr-x. 1 alumno19 alumno19   948 Jan 26 23:28 k-mer14.py
+-rw-rw-r--. 1 alumno19 alumno19   303 Jan 28 18:22 output_manejando_container.sh
+-rw-rw-r--. 1 alumno19 alumno19   492 Jan 27 00:04 slurm-9453.out
+-----APARTADO C-----
+/home/alumno19/lab5-container/data
+-----APARTADO D-----
+Python 3.9.1
+-----APARTADO E-----
+g++ (Debian 8.3.0-6) 8.3.0
+Copyright (C) 2018 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+-----APARTADO F-----
+Start
+Number of generated k-mers: 67108864
+Finish!
+
+real	0m0,955s
+user	0m0,799s
+sys	0m0,119s
